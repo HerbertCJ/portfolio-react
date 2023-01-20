35befc79ec306bffd9704cdfdf51272a598d302f
Herbert Cabus Jacby <111984596+HerbertCJ@users.noreply.github.com>
2023-01-20 09:14:58 -0300

    Created Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navbar from './components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
 function App() {
   return (
-    <div>
-      <Navbar />
+    <div>          
+      <Navbar />        
       
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,51 +1,31 @@
-import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import logo from '../images/logo.png'
 
-function NavScrollExample() {
+
+
+function CollapsibleExample() {
+  const [active, setActive] = useState(false)
+  
+
   return (
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Link</Nav.Link>
-            <NavDropdown title="Link" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Something else here
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home"><img src={logo} alt="logo" className='logo' /></Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto actived" >
+            <Nav.Link href="#features">home</Nav.Link>
+            <Nav.Link href="#pricing" >proejcts</Nav.Link>
+            <Nav.Link href="#pricing">about me</Nav.Link>
+            <Nav.Link href="#pricing">contact</Nav.Link>
           </Nav>
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
 }
 
-export default NavScrollExample;
+export default CollapsibleExample;
