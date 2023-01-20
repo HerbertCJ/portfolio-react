007b8e12630a2df55b8c0537a3e916d6afe0be2a
Herbert Cabus Jacby <111984596+HerbertCJ@users.noreply.github.com>
2023-01-20 11:50:24 -0300

    updated header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,25 +3,25 @@ import { useEffect, useRef } from 'react'
 
 
 const Header = () => {
-    // const el = useRef(null)
+    const el = useRef(null)
 
-    // useEffect(() => {
-    //     const typed = new Typed(el.current, {
-    //         strings:["HTML5", "CSS", "JavaScript", "BootStrap", "React.js"],
-    //         typeSpeed: 100,            
-    //         backSpeed: 100,
-    //         smartBackspace: true,
-    //         showCursor: false,
-    //         loop: true,
-    //     })
-    // })
+    useEffect(() => {
+        const typed = new Typed(el.current, {
+            strings:["HTML5", "CSS", "JavaScript", "BootStrap", "React.js"],
+            typeSpeed: 100,            
+            backSpeed: 100,
+            smartBackspace: true,
+            showCursor: false,
+            loop: true,
+        })
+    })
 
     return (
         <div className="header-wrapper">
             <div className="header-bg">
                 <div className="main-info">
                     <h1>web development</h1>
-                    {/*<h2 className='typed-text'><span ref={el}></span></h2>*/}
+                    <h2 className='typed-text'>- <span ref={el}></span> -</h2>
                     <a href="#" className='btn-main-offer'>contact me</a>
                 </div>
             </div>
