fb37d277bc91cdb944e7a33db9d41d525599c0d8
Herbert Cabus Jacby <111984596+HerbertCJ@users.noreply.github.com>
2023-01-20 09:16:11 -0300

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,9 @@
-import { useState } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../images/logo.png'
 
-
-
 function CollapsibleExample() {
-  const [active, setActive] = useState(false)
-  
-
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
       <Container>
@@ -17,10 +11,10 @@ function CollapsibleExample() {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto actived" >
-            <Nav.Link href="#features">home</Nav.Link>
-            <Nav.Link href="#pricing" >proejcts</Nav.Link>
-            <Nav.Link href="#pricing">about me</Nav.Link>
-            <Nav.Link href="#pricing">contact</Nav.Link>
+            <Nav.Link href="#home">home</Nav.Link>
+            <Nav.Link href="#proejcts">proejcts</Nav.Link>
+            <Nav.Link href="#aboutme">about me</Nav.Link>
+            <Nav.Link href="#contact">contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
