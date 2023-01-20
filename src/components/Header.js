37035113b1c8cab3704f7099f585ae5a70de167a
Herbert Cabus Jacby <111984596+HerbertCJ@users.noreply.github.com>
2023-01-20 11:57:39 -0300

    updated typed

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,11 +6,12 @@ const Header = () => {
     const el = useRef(null)
 
     useEffect(() => {
-        const typed = new Typed(el.current, {
+        new Typed(el.current, {
             strings:["HTML5", "CSS", "JavaScript", "BootStrap", "React.js"],
-            typeSpeed: 100,            
-            backSpeed: 100,
-            smartBackspace: true,
+            typeSpeed: 50,
+            backSpeed: 50,
+            backDelay: 500,
+            startDelay: 1000,
             showCursor: false,
             loop: true,
         })
@@ -22,7 +23,7 @@ const Header = () => {
                 <div className="main-info">
                     <h1>web development</h1>
                     <h2 className='typed-text'>- <span ref={el}></span> -</h2>
-                    <a href="#" className='btn-main-offer'>contact me</a>
+                    <button className='btn-main-offer'>contact me</button>
                 </div>
             </div>
         </div>
