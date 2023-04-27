d23cb4d4920cb66d6a1528519fcced5dc6808b1e
Herbert Cabus Jacby <herbertcj3@gmail.com>
2023-04-27 11:15:19 -0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,23 +3,33 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import logo from '../images/logo.png'
 
+
 function MyNavbar() {
+  function scrollToSection(id) {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+        inline: "nearest",
+      });
+    }
+  }
+
   return (
     <div className='navbar-bg'>
       <Navbar collapseOnSelect expand="lg" bg="dark" variant="light" fixed='top'>
         <Container>
-          <Navbar.Brand>    
-          <Nav.Link href='#home' className='nav-link' eventKey="0">
-            <img src={logo} alt="logo" /> 
-          </Nav.Link>                          
+          <Navbar.Brand>
+            <img src={logo} alt="logo" onClick={() => scrollToSection('home')} eventKey="0" style={{cursor: 'pointer'}}/>
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto actived justify-content-end" style={{width:'100%'}} >
-              <Nav.Link href='#home' className='nav-link' eventKey="1">home</Nav.Link>
-              <Nav.Link href='#projects' className='nav-link' eventKey="2" >projects</Nav.Link>
-              <Nav.Link href='#aboutme' className='nav-link' eventKey="3" >about me</Nav.Link>
-              <Nav.Link href='#contact' className='nav-link' eventKey="4" >contact</Nav.Link>           
+            <Nav className="me-auto actived justify-content-end" style={{ width: '100%' }} >
+              <Nav.Link href='' className='nav-link' eventKey="1" onClick={() => scrollToSection('home')}>home</Nav.Link>
+              <Nav.Link href='' className='nav-link' eventKey="2" onClick={() => scrollToSection('projects')} >projects</Nav.Link>
+              <Nav.Link href='' className='nav-link' eventKey="3" onClick={() => scrollToSection('aboutme')} >about me</Nav.Link>
+              <Nav.Link href='' className='nav-link' eventKey="4" onClick={() => scrollToSection('contact')} >contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
